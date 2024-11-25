752add87d28b6804c8ce50565209232d8a2fbc8a
SuciptaWijayaUNTAN <d1041211016@student.untan.ac.id>
2024-11-25 11:53:19 +0700

    change profiledesacontroller (not fixed)

--- app/Http/Controllers/ProfildesaController.php
+++ app/Http/Controllers/ProfildesaController.php
@@ -12,7 +12,9 @@ class ProfildesaController
      */
     public function index()
     {
-        return view('admin.admin-profile-desa', []);
+        return view('admin.admin-profile-desa', [
+            'profildesa' => Profildesa::first()
+        ]);
     }
 
     /**
@@ -52,7 +54,26 @@ class ProfildesaController
      */
     public function update(Request $request, Profildesa $profildesa)
     {
-        //
+        $validatedData = $request->validate([
+            'nama_desa' => 'required',
+            'sejarah_desa' => 'required',
+            'gambar_desa' => 'image',
+            'visi_desa' => 'required',
+            'misi_desa' => 'required',
+            'total_jiwa' => 'required',
+            'total_kk' => 'required',
+            'total_dusun' => 'required',
+            'total_rt' => 'required',
+            'suku' => 'required',
+            'agama' => 'required',
+            'peta_desa' => 'required',
+            'kantor_desa' => 'required',
+
+        ]);
+        Profildesa::where('id', $request->input('id'))
+            ->update($validatedData);
+
+        return redirect('/profildesa')->with('success', 'Profil Desa berhasil diupdate');
     }
 
     /**
