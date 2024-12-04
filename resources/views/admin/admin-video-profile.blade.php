5fdd08bb11e6a42c2a919e81598c3ab068ea7d2f
lia <d1041211026@student.untan.ac.id>
2024-12-04 17:30:55 +0700

    delete <hr> at admin-video-profile

--- resources/views/admin/admin-video-profile.blade.php
+++ resources/views/admin/admin-video-profile.blade.php
@@ -15,7 +15,6 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <hr>
                         <div class="card-body">
                             <form action="/videoprofile/{{ $videoprofile->id }}" method="POST">
                                 @csrf
